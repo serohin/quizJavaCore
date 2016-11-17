75211604a1854123ed60ea7f32b1c6f5a7fe9c0b
kamazz <seryegin_o@ukr.net>
2016-11-17 18:22:07 +0200

    refactor, all by zero check - through Uservalidator

--- src/main/java/com/kamazz/quiz/controller/RegisterUserController.java
+++ src/main/java/com/kamazz/quiz/controller/RegisterUserController.java
@@ -6,7 +6,7 @@ import com.kamazz.quiz.dao.datasource.JNDIDatasource;
 import com.kamazz.quiz.dao.exception.DaoSystemException;
 import com.kamazz.quiz.dao.exception.NoSuchEntityException;
 import com.kamazz.quiz.dao.interfaces.UserDao;
-import com.kamazz.quiz.entity.User;
+import com.kamazz.quiz.model.User;
 import com.kamazz.quiz.validator.UserValidator;
 
 import javax.servlet.ServletException;
@@ -53,42 +53,44 @@ public class RegisterUserController extends DependencyInjectionServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String userName = null;
-        String password = null;
-        String invite = null;
-
-        if(req.getParameter(PARAM_USERNAME)!= null & req.getParameter(PARAM_USERNAME)!= null  ){
-            userName = req.getParameter(PARAM_USERNAME).trim();
-            password = req.getParameter(PARAM_PASSWORD).trim();
-            invite = req.getParameter(PARAM_INVITE).trim();
+        String method = req.getMethod();
+        if (method.equalsIgnoreCase("GET")) {
+            req.getRequestDispatcher(PAGE_MORE_INFO).forward(req, resp);
+            return;
         }
 
-        final User user = new User(userName, password);
-        final Map<String, String> errorMap = userValidator.validate(user);
+        String userName = req.getParameter(PARAM_USERNAME);
+        String password = req.getParameter(PARAM_PASSWORD);
 
+        final User userRegister = new User(userName, password);
+        final Map<String, String> errorMapUserRegister = userValidator.validate(userRegister);
+
+        String invite = req.getParameter(PARAM_INVITE);
         if (!invite.equals(INVITE)) {
-            errorMap.put(KEY_ERROR_MAP_FALSE_INVITE, "Введите invite, который находится в моем резюме.");
+            errorMapUserRegister.put(KEY_ERROR_MAP_FALSE_INVITE, "Введите invite, который находится в моем резюме.");
         }
 
-        if (errorMap.isEmpty()) {
+        if (errorMapUserRegister.isEmpty()) {
             try (Connection conn = jndiDatasource.getDataSource().getConnection()) {
                 conn.setAutoCommit(false);
                 User tmpUser = null;
                 try {
+
                     try {
-                        tmpUser = userDao.getUserByName(user.getUsername(), conn);
+                        tmpUser = userDao.getUserByName(userRegister.getUsername(), conn);
                     } catch (NoSuchEntityException e) {/*NOP*/}
+
                     if (null != tmpUser) {
-                        errorMap.put(KEY_ERROR_MAP_USERNAME, "такой логин уже существует!");
+                        errorMapUserRegister.put(KEY_ERROR_MAP_USERNAME, "такой логин уже существует!");
                     }
-                    if (errorMap.isEmpty()) {
-                        User newUser = userDao.insertNewUser(user, conn);
+
+                    if (errorMapUserRegister.isEmpty()) {
+                        User newUser = userDao.insertNewUser(userRegister, conn);
                         HttpSession session = req.getSession(true);
                         session.setAttribute(PARAM_USER, newUser.getUsername());
                         resp.sendRedirect(REDIRECT_OK_URL);
                     } else {
-                        req.setAttribute(ATTRIBUTE_ERROR_MAP, errorMap);
+                        req.setAttribute(ATTRIBUTE_ERROR_MAP, errorMapUserRegister);
                         req.getRequestDispatcher(PAGE_MORE_INFO).forward(req, resp);
                     }
                 } catch (DaoSystemException e) {
@@ -96,24 +98,14 @@ public class RegisterUserController extends DependencyInjectionServlet {
                 }
                 conn.commit();
             } catch (SQLException e) {
-                e.printStackTrace();//убрать
                 //logger.debug(e);
             }
-
         } else {
-            req.setAttribute(ATTRIBUTE_ERROR_MAP, errorMap);
+            req.setAttribute(ATTRIBUTE_ERROR_MAP, errorMapUserRegister);
             req.setAttribute(ATTRIBUTE_INPUT_USERNAME_VALUE, userName);
             req.setAttribute(ATTRIBUTE_INPUT_PASSWORD_VALUE, password);
             req.setAttribute(ATTRIBUTE_INPUT_INVITE_VALUE, invite);
             req.getRequestDispatcher(PAGE_MORE_INFO).forward(req, resp);
-
         }
-
-    }
-
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(PAGE_MORE_INFO).forward(req, resp);
     }
 }
